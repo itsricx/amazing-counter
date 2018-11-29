c373ddbb55faba7c00cd005fb4b7ec285390d6c8
Ricardo Mejías <mejiasdoradoricardo@gmail.com>
2018-11-29 14:03:57 +0100

    Tercer commit

--- Ejercicio1Entornos/src/ejercicio1/Contador.java
+++ Ejercicio1Entornos/src/ejercicio1/Contador.java
@@ -4,8 +4,11 @@ public class Contador {
 
 	public static void main(String[] args) {
 		
+		int limite;
+		System.out.println("Introduzca un limite");
+		limite = leer.datoInt();
 		
-		for(int i = 1; i<101; i++) {
+		for(int i = 1; i<limite; i++) {
 			if(i % 2 == 0) {
 				System.out.println(i);
 				
